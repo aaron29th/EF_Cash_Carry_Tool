a72c5211b69973ae1c03f1c9345ea97371b304c3
aaron rosser <aaron25th@gmail.com>
2020-07-23 23:11:37 +0100

    Started pick classes

---
+++ CashCarryTool/Models/Pick/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eden_Farm_Cash___Carry_Tool.Models.Pick
+{
+	class Invoice
+	{
+		public int InvoiceNumber { get; set; }
+
+		public string CustomerCode { get; set; }
+		public string CustomerName { get; set; }
+
+		public DateTime DeliveryBy { get; set; }
+		public DateTime Date { get; set; }
+
+		public int Route { get; set; }
+		public int Drop { get; set; }
+
+		public string Address { get; set; }
+		public string PostCode { get; set; }
+
+		public Section Ambient { get; set; }
+		public Section Frozen { get; set; }
+		public Section Bulk { get; set; }
+	}
+}
---
+++ CashCarryTool/Models/Pick/Line.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eden_Farm_Cash___Carry_Tool.Models.Pick
+{
+	class Line
+	{
+		public string Location { get; set; }
+		public int Code { get; set; }
+		public string Description { get; set; }
+		public int Ordered { get; set; }
+
+		public string Pack { get; set; }
+		public string Size { get; set; }
+		
+		public bool Single { get; set; }
+		public bool Free { get; set; }
+		public int LineNumber { get; set; }
+		public long Barcode { get; set; }
+
+		public Line()
+		{
+			
+		}
+
+		public Line(string text)
+		{
+			
+		}
+	}
+}
