f42095743482e1ef998ec9da080307dfc757af19
aaron rosser <aaron25th@gmail.com>
2020-07-29 22:44:22 +0100

    Regex pattern for pick line

--- CashCarryTool/Models/Pick/Line.cs
+++ CashCarryTool/Models/Pick/Line.cs
@@ -17,7 +17,6 @@ namespace Eden_Farm_Cash___Carry_Tool.Models.Pick
 		public string Pack { get; set; }
 		public string Size { get; set; }
 		
-		public bool Single { get; set; }
 		public bool Free { get; set; }
 		public int LineNumber { get; set; }
 		public long Barcode { get; set; }
@@ -30,6 +29,17 @@ namespace Eden_Farm_Cash___Carry_Tool.Models.Pick
 		public Line(string text)
 		{
 			//Regex rg = new Regex(pattern);
+			var pattern = @"(^[a-zA-Z 0-9]+\/[a-zA-z0-9]+) " + // Location
+			              @"([0-9]{4,}) " + // Code
+			              @"((1 )|([0-9]+ ))" + // Pack
+			              @"((.{1,49} )|(.{50}))" + // Description
+			              @"([a-zA-Z.0-9]+) " +  // Size
+			              @"(?(4)Single |)" + // If pack == 1 then single
+			              @"((\*FREE\* )|)" + // Free?
+			              @"([0-9]+)" + // Quantity
+			              @"( _____ )" + // Del
+			              @"([0-9]+)" + // Line
+			              @"( [0-9]+|)"; // Barcode
 		}
 	}
 }
